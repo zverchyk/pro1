28746a5517f72cca479ca0d457c294f54a089b9d
zverchyk <zverchino@gmail.com>
2022-08-03 12:51:12 +0200

    first commit

---
+++ java/swiper1.js
@@ -0,0 +1,34 @@
+new Swiper('.image-slider',{
+    observer: true,
+    observerParents: true,
+    observesSlideChildren: true,
+    autoHeight: true,
+    navigation: {
+        nextE1: '.swiper-button-next',
+        prevE1: '.swiper-button-prev'
+    },
+    scrollbar:{
+        el: '.swiper-scrollbar',
+        draggable: true,
+        hide: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+
+        clickable: true,
+    },
+    mousewheel: {
+        sensitivity: 1, 
+        eventTarget: ".image-slider"
+    },
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 50000,
+        disaleOnInteraction: false,
+    },
+    speed: 800,
+    slidesPerView: 'auto',
+    
+    
+});
